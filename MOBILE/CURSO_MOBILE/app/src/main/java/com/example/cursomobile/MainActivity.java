6f473ca0fa7932ea357f998974da68bbe010b37e
Wesley <wesleygurgel27@gmail.com>
2021-01-22 11:53:34 -0300

    CURSO_MOBILE S6:21

--- MOBILE/CURSO_MOBILE/app/src/main/java/com/example/cursomobile/MainActivity.java
+++ MOBILE/CURSO_MOBILE/app/src/main/java/com/example/cursomobile/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cursomobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void alterarTexto(View view){
+
+        TextView texto = findViewById(R.id.meuTexto);
+        texto.setText("Wesley Gurgel");
+
+    }
+
 }
